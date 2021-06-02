475369afd4f06b00850ca2217b2c6a29187f69ef
AntoineCau <acauwd@gmail.com>
2021-06-02 14:58:20 +0200

    adding NavBar in the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,28 @@
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+
 function Header() {
-  return <>Ceci est mon Header</>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/my-apps">Mes Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/marketplace">Marketplace</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact">Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
+  );
 }
+
 export default Header;
