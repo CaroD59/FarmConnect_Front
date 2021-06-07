3a5f457bb1ca000eb469b124cfa3ec34cfa3f0d0
gaspard.dechirot <gaspard.balou@gmail.com>
2021-06-07 09:39:20 +0200

    having more style

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,16 +5,79 @@ function Home() {
     <StyledHome>
       <div className="profil">
         <h1> Bonjour Utilisateur </h1>
-        <ul className="infos">
-          <li> Mes coordonnées </li>
-          <li> Mon exploitation </li>
-          <li> Mes activités </li>
-        </ul>
+        <div className="infos">
+          <div className="containers">
+            <h3>Mes coordonnées</h3>
+            <ul className="coordonnées">
+              <li className="coordonnées">
+                <img
+                  className="coordonnées"
+                  src="http://simpleicon.com/wp-content/uploads/pencil.png"
+                  alt="pen"
+                />
+                06 89 67 45 67
+              </li>
+              <li className="coordonnées">
+                <img
+                  className="coordonnées"
+                  src="http://simpleicon.com/wp-content/uploads/pencil.png"
+                  alt="pen"
+                />
+                contact@champagne-toto.fr
+              </li>
+              <li className="coordonnées">
+                <img
+                  className="coordonnées"
+                  src="http://simpleicon.com/wp-content/uploads/pencil.png"
+                  alt="pen"
+                />
+                212 rue du bourg 57890 Roubaix
+              </li>
+            </ul>
+          </div>
+          <div className="containers">
+            <h3>Mon explotation</h3>
+            <ul className="content">
+              <li>
+                Forme juridique <p> Loredum lupson </p>
+              </li>
+              <li>
+                Numero sirene <p> Loredum lupson</p>
+              </li>
+              <li>
+                Numero Siret <p> Loredum lupson </p>
+              </li>
+              <li>
+                Numero TVA <p> Loredum lupson </p>
+              </li>
+              <li>
+                Numero RCS <p> Loredum lupson </p>
+              </li>
+            </ul>
+          </div>
+          <div className="containers">
+            <h3>Mes activités </h3>
+            <p> O O O</p>
+          </div>
+        </div>
       </div>
       <div className="parcelle">
         <div className="infos-parcelle">
           <h3> Parcelle </h3>
-          <p> Qte, ARe, Periode </p>
+          <ul className="parcelle">
+            <li>
+              Qte <em> 12 </em>
+            </li>
+            <li>
+              Are <em> 100 ha </em>
+            </li>
+            <li>
+              Periode <em> Ebourgeonnade </em>
+            </li>
+          </ul>
+        </div>
+        <div className="map" style={{ width: '700px', height: '300px' }}>
+          p
         </div>
       </div>
     </StyledHome>
--- src/components/Home/styles.jsx
+++ src/components/Home/styles.jsx
@@ -5,6 +5,40 @@ const StyledHome = styled.section`
   width: 100%;
   border: 1px solid black;
 
+div.parcelle {
+  display: flex;
+  justify-content: space-around;
+  width: 98%;
+  border: 1px solid red;
+  margin-left: 10px;
+  margin-top: 30px;
+ 
+ .infos-parcelle {
+   border: 1px solid grey;
+   border-radius: 3%;
+   width: 200px;
+   box-shadow: 1px 1px ;
+
+ul {
+  list-style: none;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  
+  li {
+    margin-top: 10px;
+  
+  }
+}
+
+ }
+
+  .map {
+    border-radius: 2%;
+    content: url(https://cdn2.regie-agricole.com/ulf/TNM_Biblio/fiche_135135/fiches_foncier_2013.jpg);
+  }
+}
+
   .profil {
     border: 1px solid red;
     width: 98%;
@@ -19,24 +53,79 @@ const StyledHome = styled.section`
   h1 {
     border: 1px solid yellowgreen;
     width: 98%;
+    font-family: Verdana;
+    font-weight: lighter;
+    font-size: 35px;
+    color: #7171ec;
+    margin-left: 20px;
   }
 
   .infos {
-    border: 1px solid yellowgreen;
+    
     width: 95%;
-    height: 200px;
+    height: 300px;
     display: flex;
-    flex-direction: row;
+    justify-content: space-around;
     align-items: center;
-    justify-content: space-between;
-  }
-  li {
-    border: 1px solid pink;
-    width: 300px;
-    list-style: none;
+    
+  div.containers {
+    border: 2px solid #dbd8d8;
+    box-shadow: 1px 1px #dbd8d8;
+    width: 400px;
     text-align: center;
-    height: 185px;
+    height: 250px;
     margin-right: 35px;
+    border-radius: 4%;
+    h3 {
+      font-family: Verdana;
+    font-weight: normal;
+    font-size: 20px;
+    text-align: left;
+    padding: 10px;
+    
+    }
+ul.coordonnées {
+  border: 1px solid yellowgreen;
+  height: 150px;
+  margin-top: 0;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+
+li.coordonnées {
+  list-style: none;
+        font-family: Arial, Helvetica, sans-serif;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        width: 290px;
+        height: 30px;
+        img {
+          width: 20px;
+          height: 20px;
+          margin-left: 0;
+        }
+}
+}
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      text-align: start;
+      li {
+        list-style: none;
+        font-family: Arial, Helvetica, sans-serif;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        border: 1px solid red;
+        width: 290px;
+        height: 30px;
+        p {
+          font-weight: bold;
+        }
+      }
+    }
   }
 `;
 
