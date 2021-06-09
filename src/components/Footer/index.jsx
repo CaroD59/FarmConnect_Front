f45814468c962352495c04ed66a81eb48dce1a6a
AntoineCau <acauwd@gmail.com>
2021-06-09 09:34:25 +0200

    adding style on footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,22 @@
 function Footer() {
   return (
     <>
-      <div className="Header">Ceci est mon Footer </div>
-      <div className="legal_information" />
-      <ul>
-        <li>Politique de Confidientalité</li>
-        <li>Mentions Légales</li>
-        <li>CGU</li>
-      </ul>
-      <div className="contact" />
-      <ul>
-        <li>Qui sommes-nous ?</li>
-        <li>On parle de nous</li>
-        <li>Contactez-nous</li>
-      </ul>
+      <div className="Header">
+        <div className="legal_information">
+          <ul>
+            <li>Politique de Confidientalité</li>
+            <li>Mentions Légales</li>
+            <li>CGU</li>
+          </ul>
+        </div>
+        <div className="contact">
+          <ul>
+            <li>Qui sommes-nous ?</li>
+            <li>On parle de nous</li>
+            <li>Contactez-nous</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Footer/styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.div`
+  .Header {
+    display: flex;
+    justify-content: flex-start;
+    border: black solid 2px;
+    .footer_information {
+      list-style-type: none;
+      border: black solid 2px;
+    }
+  }
+`;
+
+export default StyledFooter;
