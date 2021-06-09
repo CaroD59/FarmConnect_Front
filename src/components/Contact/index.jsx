38ca57ebfc8d270f5fae7a868e4435e2fbf25aaa
AntoineCau <acauwd@gmail.com>
2021-06-09 16:03:41 +0200

    Deleting evils

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,13 +1,9 @@
-import Header from '../Header/index';
-import Footer from '../Footer/index';
 import StyledContact from './styles';
 
 function Contact() {
   return (
     <StyledContact>
-      <Header />
       <h1> Contact Section </h1>
-      <Footer />
     </StyledContact>
   );
 }
--- src/components/MyApps/index.jsx
+++ src/components/MyApps/index.jsx
@@ -1,13 +1,9 @@
-import Header from '../Header/index';
-import Footer from '../Footer/index';
 import StyledMyApps from './styles';
 
 function MyApps() {
   return (
     <StyledMyApps>
-      <Header />
       <h1> There are my services </h1>
-      <Footer />
     </StyledMyApps>
   );
 }
