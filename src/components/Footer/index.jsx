348fb1b6b7d8aa7a41d3fa7687715ec1dad1f96c
AntoineCau <acauwd@gmail.com>
2021-06-09 09:17:19 +0200

    adding structure for the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,20 @@
 function Footer() {
-  return <>Ceci est mon Footer </>;
+  return (
+    <>
+      <div className="Header">Ceci est mon Footer </div>
+      <div className="legal_information" />
+      <ul>
+        <li>Politique de Confidientalité</li>
+        <li>Mentions Légales</li>
+        <li>CGU</li>
+      </ul>
+      <div className="contact" />
+      <ul>
+        <li>Qui sommes-nous ?</li>
+        <li>On parle de nous</li>
+        <li>Contactez-nous</li>
+      </ul>
+    </>
+  );
 }
 export default Footer;
