6f0f8917d72cc59b42d67183bc805055cefce3a6
AntoineCau <acauwd@gmail.com>
2021-06-10 08:01:00 +0200

    adding structure of the form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,22 @@
 function Contact() {
-  return <h1> Contact Section </h1>;
+  return (
+    <>
+      <div className="contactForm">
+        <form>
+          <label htmlFor="name">
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <label htmlFor="email">
+            Mail:
+            <input type="text" name="email" />
+          </label>
+
+          <input type="submit" value="Envoyer" />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
