c59f597525ce7b5675780f26a3a64d9fb43c0bae
gaspard.dechirot <gaspard.balou@gmail.com>
2021-06-11 16:01:40 +0200

    last changes for home style because after a client request we decided not to do more because of the cockpit witch is a none defined notion

--- src/components/Home/styles.jsx
+++ src/components/Home/styles.jsx
@@ -10,120 +10,126 @@ const StyledHome = styled.section`
     font-family: 'roboto', sans-serif;
     background-color: rgba(218, 218, 218, 0.25);
     .profil {
-      
       display: flex;
       width: 100%;
       .infos {
-      height: 300px;
-      display: flex;
-      width: 100%;
-      justify-content: center;
-      .containers {
-        padding: 5px;
-        margin: 20px;
-        width: 25vw;
         height: 300px;
-        border-radius: 8px;
-        background-color: #ffff;
-        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
-        .picto {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          padding-top: 8vh;
-          .pic {
-          margin: 5px;
-          width: 70px;
-          height: 70px;
-          border-radius: 100%;
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        .containers {
+          padding: 5px;
+          margin: 20px;
+          width: 25vw;
+          height: 300px;
+          border-radius: 8px;
+          background-color: #ffff;
           box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        }
-        
-        .cardName {
-          font-size: 20px;
-          padding: 15px;
-          margin: 0;
-          font-weight: normal;
-        }
-        .card {
-          height: 200px;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          padding: 10px;
-          margin: 0;
-          li {
-            list-style: none;
-            text-align: start;
+          .picto {
             display: flex;
+            justify-content: center;
             align-items: center;
-            justify-content: space-between;
-            p {
-              margin: 0;
-              font-weight: bold;
+            padding-top: 8vh;
+            .pic {
+              margin: 5px;
+              width: 70px;
+              height: 70px;
+              border-radius: 100%;
+              box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+              display: flex;
+              justify-content: center;
+              align-items: center;
             }
           }
-          }
-        }
-      }
-    }
 
-    }
-    .parcelle {
-      width: 91%;
-      height: 400px;
-      display: flex;
-      justify-content: space-around;
-      margin: 70px;
-      
-      .infos-parcelle {
-        width: 250px;
-        margin: 30px;
-        border-radius: 8px;
-        background-color: #ffff;
-        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
-        h3 {
-          font-size: 20px;
-          padding: 15px;
-          ul {
+          .cardName {
+            font-size: 20px;
+            padding: 15px;
+            margin: 0;
+            font-weight: normal;
+          }
+          .card {
+            height: 200px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            padding: 10px;
+            margin: 0;
             li {
+              list-style: none;
+              text-align: start;
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+              p {
+                margin: 0;
+                font-weight: bold;
+              }
             }
           }
         }
+      }
     }
-      .parcelleMap {
-        width: 800px;
-        margin: 30px;
-        border-radius: 8px;
-        background-color: #ffff;
-        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
-        p {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100%;
+  }
+  .parcelle {
+    width: 91%;
+    height: 400px;
+    display: flex;
+    justify-content: space-around;
+    margin: 70px;
+
+    .infos-parcelle {
+      width: 250px;
+      margin: 30px;
+      border-radius: 8px;
+      background-color: #ffff;
+      box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+      h3 {
+        font-size: 28px;
+        padding: 15px;
+        margin-top: 20px;
+      }
+      ul {
+        margin-top: 40px;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        li {
+          padding-left: 10px;
+          em {
+            font-weight: bold;
+          }
         }
       }
     }
-     
-    .helloUser {
-      color: rgba(70, 70, 70, 1);
-      text-align: center;
-      font-size: 15px;
+    .parcelleMap {
+      width: 800px;
       margin: 30px;
-      padding: 10px;
+      border-radius: 8px;
+      background-color: #ffff;
+      box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+      p {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+      }
     }
-    .welcomeProfil {
-      color: rgba(70, 70, 70, 1);
-      text-align: center;
-      font-size: 40px;
-      margin: 30px;
-      padding: 10px;
-    }
+  }
+  .helloUser {
+    color: rgba(70, 70, 70, 1);
+    text-align: center;
+    font-size: 15px;
+    margin: 30px;
+    padding: 10px;
+  }
+  .welcomeProfil {
+    color: rgba(70, 70, 70, 1);
+    text-align: center;
+    font-size: 40px;
+    margin: 30px;
+    padding: 10px;
   }
 `;
 
