0e413b442035933bc7d69f458686e24bd8d6c109
gaspard.dechirot <gaspard.balou@gmail.com>
2021-06-11 14:45:50 +0200

    having style

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,20 +4,34 @@ function Home() {
   return (
     <StyledHome>
       <div className="home">
+        <h1 className="helloUser"> Bonjour Jean Claude ! </h1>
+        <h2 className="welcomeProfil">Bienvenue sur votre profil.</h2>
         <div className="profil">
-          <h1 className="helloUser"> Bonjour Jean Claude ! </h1>
-          <h2 className="welcomeProfil">Bienvenue sur votre profil.</h2>
           <div className="infos">
             <div className="containers">
               <h3 className="cardName">Mes coordonnées</h3>
               <ul className="card">
-                <li>Nom : Claude</li>
-                <li>Prénom : Jean</li>
-                <li>Téléphone : 06 89 67 45 67</li>
-                <li>Mail : contact@champagne-toto.fr</li>
-                <li>Adresse : 212 rue du bourg</li>
-                <li>Code Postal : 57890</li>
-                <li>Ville : Roubaix</li>
+                <li>
+                  Nom : <p> Claude </p>
+                </li>
+                <li>
+                  Prénom : <p> Jean </p>
+                </li>
+                <li>
+                  Téléphone : <p> 06 89 67 45 67 </p>
+                </li>
+                <li>
+                  Mail : <p> contact@champagne-toto.fr </p>
+                </li>
+                <li>
+                  Adresse : <p> 212 rue du bourg </p>
+                </li>
+                <li>
+                  Code Postal : <p> 57890 </p>
+                </li>
+                <li>
+                  Ville : <p> Roubaix </p>
+                </li>
               </ul>
             </div>
             <div className="containers">
@@ -42,7 +56,40 @@ function Home() {
             </div>
             <div className="containers">
               <h3 className="cardName">Mes activités </h3>
-              <p> O O O</p>
+              <div className="picto">
+                <div className="pic">
+                  <img
+                    src="https://img.icons8.com/cotton/452/grape.png"
+                    alt=""
+                    style={{
+                      width: '60px',
+                      height: '60px',
+                    }}
+                  />
+                </div>
+                <div className="pic">
+                  <img
+                    src="https://static.vecteezy.com/ti/vecteur-libre/p1/2136060-feuille-logo-bio-label-eco-icon-vector-isolated-background-gratuit-vectoriel.jpg"
+                    alt=""
+                    style={{
+                      width: '60px',
+                      height: '60px',
+                      borderRadius: '100%',
+                    }}
+                  />
+                </div>
+                <div className="pic">
+                  <img
+                    src="https://us.123rf.com/450wm/dstarky/dstarky1702/dstarky170200652/71441287-icône-de-vache-ou-d-un-logo-dans-le-style-de-ligne-moderne-haute-qualité-contour-noir-pictogramme-pour.jpg?ver=6"
+                    alt=""
+                    style={{
+                      width: '70px',
+                      height: '70px',
+                      borderRadius: '100%',
+                    }}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Home/styles.jsx
+++ src/components/Home/styles.jsx
@@ -10,69 +10,39 @@ const StyledHome = styled.section`
     font-family: 'roboto', sans-serif;
     background-color: rgba(218, 218, 218, 0.25);
     .profil {
-      width: 60%;
-    }
-    .parcelle {
-      width: 60%;
-      height: 500px;
+      
       display: flex;
-      justify-content: space-between;
-      margin: 30px 80px;
-      .parcelleMap {
-        width: 80%;
-        margin: 30px;
-        border-radius: 8px;
-        background-color: #ffff;
-        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
-        p {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100%;
-        }
-      }
-    }
-      .infos-parcelle {
-        width: 20%;
-        margin: 30px;
-        border-radius: 8px;
-        background-color: #ffff;
-        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
-        h3 {
-          font-size: 20px;
-          padding: 15px;
-          ul {
-            li {
-            }
-          }
-        }
-    }
-    .helloUser {
-      color: rgba(70, 70, 70, 1);
-      text-align: center;
-      font-size: 15px;
-      margin: 30px;
-      padding: 10px;
-    }
-    .welcomeProfil {
-      color: rgba(70, 70, 70, 1);
-      text-align: center;
-      font-size: 40px;
-      margin: 30px;
-      padding: 10px;
-    }
-    .infos {
+      width: 100%;
+      .infos {
       height: 300px;
       display: flex;
-      justify-content: space-between;
-      align-items: center;
+      width: 100%;
+      justify-content: center;
       .containers {
-        margin: 30px;
-        width: 500px;
+        padding: 5px;
+        margin: 20px;
+        width: 25vw;
         height: 300px;
         border-radius: 8px;
         background-color: #ffff;
         box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+        .picto {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding-top: 8vh;
+          .pic {
+          margin: 5px;
+          width: 70px;
+          height: 70px;
+          border-radius: 100%;
+          box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        }
+        
         .cardName {
           font-size: 20px;
           padding: 15px;
@@ -91,13 +61,68 @@ const StyledHome = styled.section`
             text-align: start;
             display: flex;
             align-items: center;
+            justify-content: space-between;
             p {
               margin: 0;
+              font-weight: bold;
             }
           }
           }
         }
       }
+    }
+
+    }
+    .parcelle {
+      width: 91%;
+      height: 400px;
+      display: flex;
+      justify-content: space-around;
+      margin: 70px;
+      
+      .infos-parcelle {
+        width: 250px;
+        margin: 30px;
+        border-radius: 8px;
+        background-color: #ffff;
+        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+        h3 {
+          font-size: 20px;
+          padding: 15px;
+          ul {
+            li {
+            }
+          }
+        }
+    }
+      .parcelleMap {
+        width: 800px;
+        margin: 30px;
+        border-radius: 8px;
+        background-color: #ffff;
+        box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.1);
+        p {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100%;
+        }
+      }
+    }
+     
+    .helloUser {
+      color: rgba(70, 70, 70, 1);
+      text-align: center;
+      font-size: 15px;
+      margin: 30px;
+      padding: 10px;
+    }
+    .welcomeProfil {
+      color: rgba(70, 70, 70, 1);
+      text-align: center;
+      font-size: 40px;
+      margin: 30px;
+      padding: 10px;
     }
   }
 `;
