e9bee3b2f4bf2a771c1da9d83c2776a03b7a7583
AntoineCau <acauwd@gmail.com>
2021-06-16 18:37:39 +0200

    ApplicationPage is functional

--- src/components/ApplicationPage/index.jsx
+++ src/components/ApplicationPage/index.jsx
@@ -4,17 +4,27 @@ import { useParams } from 'react-router-dom';
 import StyledApplicationPage from './styles';
 
 function ApplicationPage() {
-  const [application, setApplication] = useState([]);
+  const [application, setApplication] = useState(null);
   const { id } = useParams();
   useEffect(() => {
     axios.get(`http://localhost:5050/apps/${id}`).then(({ data }) => {
-      setApplication(data[0]);
+      setApplication(data);
     });
   }, []);
   return (
     <StyledApplicationPage>
-      <h2>Application</h2>
-      <p>{application.name}</p>
+      {application && (
+        <>
+          <div className="logoBanner">
+            <img src={application.logo} alt="" className="logo" />
+            <img src={application.banner} alt="" className="banner" />
+          </div>
+          <div className="nameDesc">
+            <h2 className="name">{application.name}</h2>
+            <p className="description">{application.description}</p>
+          </div>
+        </>
+      )}
     </StyledApplicationPage>
   );
 }
--- src/components/ApplicationPage/styles.jsx
+++ src/components/ApplicationPage/styles.jsx
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
-const StyledApplicationPage = styled.div``;
+const StyledApplicationPage = styled.div`
+  .logoBanner {
+    position: relative;
+    width: 100vw;
+    height: 350px;
+    display: flex;
+    justify-content: center;
+    border: black solid 2px;
+    .logo {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100px;
+      height: 100px;
+    }
+    .banner {
+      max-width: 100%;
+    }
+  }
+  .nameDesc {
+    width: 100vw;
+    height: 500px;
+    color: #223594;
+    font-family: 'roboto', sans-serif;
+    border: yellowgreen solid 2px;
+    .name {
+      margin-top: 20px;
+      margin-left: 160px;
+      font-size: 45px;
+    }
+    .description {
+      margin-top: 20px;
+      margin-left: 300px;
+    }
+  }
+`;
 
 export default StyledApplicationPage;
