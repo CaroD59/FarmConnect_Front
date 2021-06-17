88cf03a7317532ee47740a7f585364cad066eede
CaroD59 <dorchies.c@gmail.com>
2021-06-17 12:22:55 +0200

    Style General Catalogue

--- src/components/MarketPlaceCatalog/index.jsx
+++ src/components/MarketPlaceCatalog/index.jsx
@@ -17,13 +17,18 @@ export default function GeneralCatalogue() {
     });
   }, []);
   return (
-    <div onClick={appHandler} onKeyDown={appHandler} aria-hidden="true">
+    <div>
       <StyledMarketPlaceCatalog>
         <h1 className="titre">Catalogue</h1>
         <div className="AppGallery">
           {generalCatalogue.map((catalogue) => {
             return (
-              <div className="card">
+              <div
+                className="card"
+                onClick={appHandler}
+                onKeyDown={appHandler}
+                aria-hidden="true"
+              >
                 <div className="image">
                   <img
                     src={catalogue.logo}
