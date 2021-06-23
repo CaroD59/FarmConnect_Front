9baf3d1719916bad24623b927cb867c0da320ac7
AntoineCau <acauwd@gmail.com>
2021-06-23 11:04:07 +0200

    initialization and basic structures

---
+++ src/components/Articles/index.jsx
@@ -0,0 +1,20 @@
+import StyledArticles from './styles';
+
+function Articles() {
+  return (
+    <StyledArticles>
+      <div className="articles">
+        <h1> Articles Section </h1>
+        <ul>
+          <li>Article 1</li>
+          <li>Article 2</li>
+          <li>Article 3</li>
+          <li>Article 4</li>
+          <li>Article 5</li>
+        </ul>
+      </div>
+    </StyledArticles>
+  );
+}
+
+export default Articles;
