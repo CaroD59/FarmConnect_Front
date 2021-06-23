8c8ae6a4ea960018bcded6d3820e6285857b5b47
AntoineCau <acauwd@gmail.com>
2021-06-23 10:19:24 +0200

    initialization

---
+++ src/components/AboutUs/index.jsx
@@ -0,0 +1,11 @@
+import StyledAboutUs from './styles';
+
+function AboutUs() {
+  return (
+    <StyledAboutUs>
+      <h1> About Us Section </h1>
+    </StyledAboutUs>
+  );
+}
+
+export default AboutUs;
