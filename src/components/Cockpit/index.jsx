d0510e5a251548464f73a2a7a414a12da7850067
AntoineCau <acauwd@gmail.com>
2021-06-23 11:47:28 +0200

    Initialization of Cockpit Page

---
+++ src/components/Cockpit/index.jsx
@@ -0,0 +1,11 @@
+import StyledCockpit from './styles';
+
+function Cockpit() {
+  return (
+    <StyledCockpit>
+      <h1> Cockpit Section </h1>
+    </StyledCockpit>
+  );
+}
+
+export default Cockpit;
