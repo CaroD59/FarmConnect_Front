c583d55230ad152d8ecfcd2bfde123ad4fe4f74e
erriep <tabary.pierre.dev@gmail.com>
2021-07-07 14:56:21 +0200

    resolve bug

--- src/components/MyApps/index.jsx
+++ src/components/MyApps/index.jsx
@@ -8,47 +8,45 @@ function MyApps() {
   const [myApps, setMyApps] = useState([]);
   const { user } = useContext(User);
   const history = useHistory();
-  console.log(user);
 
   useEffect(() => {
-    axios.get(`http://localhost:5051/appfav/${user.id}`).then(({ data }) => {
-      setMyApps(data);
-    });
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/appfav/${user.id}`)
+      .then(({ data }) => {
+        setMyApps(data);
+      });
   }, []);
 
-  console.log(myApps);
   return (
     <StyledMarketPlaceCatalog>
       <h1 className="titre">Mes applications</h1>
       <div className="AppGallery">
         {myApps.map((catalogue) => {
           return (
-            <>
-              <div
-                className="card"
-                onClick={(e) => {
-                  e.preventDefault();
-                  history.push(`/apps/${catalogue.id}`);
-                }}
-                aria-hidden="true"
-                key={catalogue.id}
-              >
-                <div className="image">
-                  <img
-                    src={catalogue.banner}
-                    alt="banner"
-                    className="imageSize"
-                  />
-                </div>
-                <div className="infos">
-                  <div className="societe">
-                    <p className="name">{catalogue.name}</p>
-                    <p className="activite">{catalogue.provider_app}</p>
-                  </div>
-                  <div className="button">Infos</div>
-                </div>
+            <div
+              className="card"
+              onClick={(e) => {
+                e.preventDefault();
+                history.push(`/apps/${catalogue.id}`);
+              }}
+              aria-hidden="true"
+              key={catalogue.id}
+            >
+              <div className="image">
+                <img
+                  src={catalogue.banner}
+                  alt="banner"
+                  className="imageSize"
+                />
               </div>
-            </>
+              <div className="infos">
+                <div className="societe">
+                  <p className="name">{catalogue.name}</p>
+                  <p className="activite">{catalogue.provider_app}</p>
+                </div>
+                <div className="button">Infos</div>
+              </div>
+            </div>
           );
         })}
       </div>
