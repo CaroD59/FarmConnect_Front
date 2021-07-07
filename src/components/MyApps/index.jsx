44b9d09bc77123abeaa4739188388224d99a626d
erriep <tabary.pierre.dev@gmail.com>
2021-07-07 14:17:16 +0200

    add useContexts for id user && add applications of user on myservices

--- src/components/MyApps/index.jsx
+++ src/components/MyApps/index.jsx
@@ -1,10 +1,58 @@
-import StyledMyApps from './styles';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import StyledMarketPlaceCatalog from '../MarketPlaceCatalog/styles';
+import User from '../../contexts/User';
 
 function MyApps() {
+  const [myApps, setMyApps] = useState([]);
+  const { user } = useContext(User);
+  const history = useHistory();
+  console.log(user);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5051/appfav/${user.id}`).then(({ data }) => {
+      setMyApps(data);
+    });
+  }, []);
+
+  console.log(myApps);
   return (
-    <StyledMyApps>
-      <h1> There are my services </h1>
-    </StyledMyApps>
+    <StyledMarketPlaceCatalog>
+      <h1 className="titre">Mes applications</h1>
+      <div className="AppGallery">
+        {myApps.map((catalogue) => {
+          return (
+            <>
+              <div
+                className="card"
+                onClick={(e) => {
+                  e.preventDefault();
+                  history.push(`/apps/${catalogue.id}`);
+                }}
+                aria-hidden="true"
+                key={catalogue.id}
+              >
+                <div className="image">
+                  <img
+                    src={catalogue.banner}
+                    alt="banner"
+                    className="imageSize"
+                  />
+                </div>
+                <div className="infos">
+                  <div className="societe">
+                    <p className="name">{catalogue.name}</p>
+                    <p className="activite">{catalogue.provider_app}</p>
+                  </div>
+                  <div className="button">Infos</div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </StyledMarketPlaceCatalog>
   );
 }
 
