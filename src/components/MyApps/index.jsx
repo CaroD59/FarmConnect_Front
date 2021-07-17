40ee90bc4a05156ded05ec8229a4c800617ec7d3
erriep <tabary.pierre.dev@gmail.com>
2021-07-17 23:51:16 +0200

    update mycockpit myapps myprofil

--- src/components/MyApps/index.jsx
+++ src/components/MyApps/index.jsx
@@ -7,42 +7,54 @@ import User from '../../contexts/User';
 function MyApps() {
   const [myApps, setMyApps] = useState([]);
   const { user } = useContext(User);
-
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/appfav/${user.id}`)
-      .then(({ data }) => {
-        setMyApps(data);
-      });
-  }, []);
+  if (user !== null) {
+    useEffect(() => {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/appfav/${user.id}`)
+        .then(({ data }) => {
+          setMyApps(data);
+        });
+    }, []);
+  }
 
   return (
     <StyledMarketPlaceCatalog>
-      <h1 className="titre">Mes applications</h1>
-      <div className="AppGallery">
-        {myApps.map((catalogue) => {
-          return (
-            <Link to={`/${catalogue.id}/${catalogue.name}`}>
-              <div className="card" aria-hidden="true" key={catalogue.id}>
-                <div className="image">
-                  <img
-                    src={catalogue.banner}
-                    alt="banner"
-                    className="imageSize"
-                  />
-                </div>
-                <div className="infos">
-                  <div className="societe">
-                    <p className="name">{catalogue.name}</p>
-                    <p className="activite">{catalogue.provider_app}</p>
+      {user ? (
+        <div className="AppGallery">
+          <h1 className="titre">Mes applications</h1>
+          {myApps.map((catalogue) => {
+            return (
+              <Link to={`/${catalogue.id}/${catalogue.name}`}>
+                <div className="card" aria-hidden="true" key={catalogue.id}>
+                  <div className="image">
+                    <img
+                      src={catalogue.banner}
+                      alt="banner"
+                      className="imageSize"
+                    />
                   </div>
-                  <div className="button">Infos</div>
+                  <div className="infos">
+                    <div className="societe">
+                      <p className="name">{catalogue.name}</p>
+                      <p className="activite">{catalogue.provider_app}</p>
+                    </div>
+                    <div className="button">Infos</div>
+                  </div>
                 </div>
-              </div>
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="userOff">
+          <div className="withoutUser">
+            <h2 className="titleProfile">Vous n&apos;êtes pas connecté</h2>
+            <Link to="/" className="buttonProfil">
+              se connecter
             </Link>
-          );
-        })}
-      </div>
+          </div>
+        </div>
+      )}
     </StyledMarketPlaceCatalog>
   );
 }
