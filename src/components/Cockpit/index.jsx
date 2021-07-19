0352376583c4b120a8cd95f41e6330ca38e01787
AntoineCau <acauwd@gmail.com>
2021-07-19 16:51:37 +0200

    style on cockpit

--- src/components/Cockpit/index.jsx
+++ src/components/Cockpit/index.jsx
@@ -25,6 +25,59 @@ function Cockpit() {
   return (
     <StyledCockpit>
       <div className="cockpit">
+        <div className="container">
+          <div className="profil">
+            <div>
+              <h2 className="titleProfile center">PROFIL</h2>
+              {completed && (
+                <p className="completed center">Votre profil est complété</p>
+              )}
+              {!completed && (
+                <p className="notCompleted center">
+                  Votre profil est incomplet
+                </p>
+              )}
+            </div>
+            <div className="center">
+              <Link to="/myprofile" className="contactButtonProfil">
+                Accéder à mon profil
+              </Link>
+            </div>
+          </div>
+          <div className="myapps">
+            <div>
+              <h2 className="titleMyApps center">APPLICATIONS</h2>
+              {myApps.length !== 0 && (
+                <p className="completed center">
+                  Vous avez {myApps.length} applications enregistrées
+                </p>
+              )}
+              {myApps.length === 0 && (
+                <p className="notCompleted">
+                  Vous n&apos;avez pas encore d&apos;applications
+                </p>
+              )}
+            </div>
+            <div className="center">
+              {myApps.length !== 0 && (
+                <Link to="/myapps" className="contactButtonApp">
+                  Accéder à mes applications
+                </Link>
+              )}
+              <Link to="/apps" className="contactButtonApp">
+                Accéder à la Martketplace
+              </Link>
+            </div>
+          </div>
+          <div className="news">
+            <div>
+              <h2 className="titleNews center">NEWS</h2>
+              <p className="newsText center">
+                Futures news autour de Farm Connect
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="beta">
           <p>Farm Connect est actuellement en version &quot;BETA&quot;.</p>
           <p>
@@ -34,44 +87,6 @@ function Cockpit() {
           <Link to="/contact" className="contactButton">
             Donner mon avis
           </Link>
-        </div>
-        <div className="container">
-          <div className="profil">
-            <h2 className="titleProfile">PROFIL</h2>
-            {completed && (
-              <p className="completed">Votre profil est complété !</p>
-            )}
-            {!completed && (
-              <p className="notCompleted">Votre profil est incomplet !</p>
-            )}
-            <Link to="/myprofile" className="contactButtonProfil">
-              Cliquer ici pour accéder à mon profil
-            </Link>
-          </div>
-          <div className="myapps">
-            <h2 className="titleMyApps">Applications</h2>
-            {myApps.length !== 0 && (
-              <p className="completed">
-                Vous avez {myApps.length} applications
-              </p>
-            )}
-            {myApps.length === 0 && (
-              <p className="notCompleted">
-                Vous n&apos;avez pas encore d&apos;applications
-              </p>
-            )}
-            {myApps.length !== 0 && (
-              <Link to="/myapps" className="contactButtonApp">
-                Cliquer ici pour accéder à vos applications
-              </Link>
-            )}
-            <Link to="/apps" className="contactButtonApp">
-              Cliquer ici pour accéder au catalogue d&apos;applications
-            </Link>
-          </div>
-          <div className="soon">
-            <h2 className="titleProfile">Bientôt disponible</h2>
-          </div>
         </div>
       </div>
     </StyledCockpit>
--- src/components/Cockpit/styles.jsx
+++ src/components/Cockpit/styles.jsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 const StyledCockpit = styled.div`
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column wrap;
+  }
   .cockpit {
     width: 100vw;
-    height: 80vh;
-    padding-top: 5vh;
     display: flex;
     justify-content: start;
     align-items: center;
@@ -15,12 +19,12 @@ const StyledCockpit = styled.div`
       justify-content: center;
       align-items: center;
       flex-direction: column;
-      margin-bottom: 5vh;
-      padding-top: 20px;
+      margin-bottom: 30px;
+      padding-top: 30px;
       width: 80vw;
-      height: 100px;
       border-radius: 3px;
       background-color: #ffff;
+      box-shadow: -1px -1px 21px 1px rgba(70, 70, 70, 0.15);
       p {
         margin: 5px;
       }
@@ -30,8 +34,8 @@ const StyledCockpit = styled.div`
         align-items: center;
         width: 150px;
         height: 50px;
-        margin-top: 10px;
-        margin-bottom: 15px;
+        margin-top: 20px;
+        margin-bottom: 25px;
         color: #ffff;
         background-color: rgb(247, 143, 33, 0.75);
         border: solid 1px rgb(247, 143, 33, 0.75);
@@ -44,25 +48,30 @@ const StyledCockpit = styled.div`
     .container {
       display: flex;
       justify-content: space-between;
+      margin-top: 80px;
       width: 80vw;
-      height: 40vh;
+      height: 60vh;
       .profil {
         width: 25vw;
-        margin: 25px 0px 25px 0px;
+        margin-bottom: 35px;
         display: flex;
-        justify-content: space-around;
-        align-items: center;
         flex-direction: column;
+        justify-content: space-between;
+        align-items: stretch;
         border-radius: 5px;
         background-color: #ffff;
+        box-shadow: -1px -1px 21px 1px rgba(70, 70, 70, 0.15);
         .titleProfile {
           color: #223594;
+          margin-top: 30px;
           font-size: 30px;
         }
         .completed {
-          color: green;
+          padding-top: 50px;
+          color: rgba(70, 70, 70, 1);
         }
         .notCompleted {
+          padding-top: 50px;
           color: red;
         }
         .contactButtonProfil {
@@ -72,7 +81,7 @@ const StyledCockpit = styled.div`
           width: 80%;
           height: 50px;
           margin-top: 10px;
-          margin-bottom: 15px;
+          margin-bottom: 25px;
           color: #ffff;
           background-color: rgb(247, 143, 33, 0.75);
           border: solid 1px rgb(247, 143, 33, 0.75);
@@ -84,21 +93,25 @@ const StyledCockpit = styled.div`
       }
       .myapps {
         width: 25vw;
-        margin: 25px 0px 25px 0px;
+        margin-bottom: 35px;
         display: flex;
-        justify-content: space-around;
-        align-items: center;
         flex-direction: column;
+        justify-content: space-between;
+        align-items: stretch;
         border-radius: 5px;
         background-color: #ffff;
+        box-shadow: -1px -1px 21px 1px rgba(70, 70, 70, 0.15);
         .titleMyApps {
           color: #223594;
           font-size: 30px;
+          margin-top: 30px;
         }
         .completed {
-          color: green;
+          padding-top: 50px;
+          color: rgba(70, 70, 70, 1);
         }
         .notCompleted {
+          padding-top: 50px;
           color: red;
         }
         .contactButtonApp {
@@ -108,7 +121,7 @@ const StyledCockpit = styled.div`
           width: 80%;
           height: 50px;
           margin-top: 10px;
-          margin-bottom: 15px;
+          margin-bottom: 25px;
           color: #ffff;
           background-color: rgb(247, 143, 33, 0.75);
           border: solid 1px rgb(247, 143, 33, 0.75);
@@ -118,35 +131,23 @@ const StyledCockpit = styled.div`
           text-decoration: none;
         }
       }
-      .soon {
+      .news {
         width: 25vw;
-        margin: 25px 0px 25px 0px;
+        margin-bottom: 35px;
         display: flex;
-        justify-content: space-around;
         align-items: center;
         flex-direction: column;
         border-radius: 5px;
         background-color: #ffff;
-        .titleMyApps {
+        box-shadow: -1px -1px 21px 1px rgba(70, 70, 70, 0.15);
+        .titleNews {
           color: #223594;
           font-size: 30px;
+          margin-top: 30px;
         }
-
-        .contactButtonApp {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 80%;
-          height: 50px;
-          margin-top: 10px;
-          margin-bottom: 15px;
-          color: #ffff;
-          background-color: rgb(247, 143, 33, 0.75);
-          border: solid 1px rgb(247, 143, 33, 0.75);
-          border-radius: 5px;
-          cursor: pointer;
-          font-size: 15px;
-          text-decoration: none;
+        .newsText {
+          padding-top: 50px;
+          color: rgba(70, 70, 70, 1);
         }
       }
     }
