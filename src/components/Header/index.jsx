a40e53a7053dc36185ea02b33d34f8a811f3893c
gaspard.dechirot <gaspard.balou@gmail.com>
2021-07-20 15:58:55 +0200

    ok

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -35,6 +35,24 @@ function Header() {
           </div>
         </div>
       </nav>
+      <nav className="Burger">
+        <div className="PagesBurger">
+          <ul>
+            <li className="Cockpit">
+              <NavLink to="/cockpit">Cockpit</NavLink>
+            </li>
+            <li className="MyApps">
+              <NavLink to="/myapps">Mes Services</NavLink>
+            </li>
+            <li className="Marketplace">
+              <NavLink to="/apps">Marketplace</NavLink>
+            </li>
+            <li className="Contact">
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </StyledHeader>
   );
 }
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -12,6 +12,9 @@ const StyledHeader = styled.div`
     background-color: #ffff;
     border-bottom: rgb(247, 143, 33, 0.75) 1px solid;
     box-shadow: 0px 10px 10px rgba(218, 218, 218, 0.25);
+    .burger {
+      display: none;
+    }
     .logoContainer {
       a {
       }
@@ -49,6 +52,53 @@ const StyledHeader = styled.div`
               padding: 10px;
               border-bottom: rgba(34, 53, 148, 1) solid 2px;
               transition: 0.2s;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 920px) {
+    .NavBar {
+      display: none;
+      .container {
+        display: flex;
+        .Burger {
+          position: fixed;
+          top: 0;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 100vw;
+          height: 80px;
+          background-color: #ffff;
+          border-bottom: rgb(247, 143, 33, 0.75) 1px solid;
+          box-shadow: 0px 10px 10px rgba(218, 218, 218, 0.25);
+          .PagesBurger {
+            display: flex;
+            align-items: center;
+            ul {
+              display: flex;
+              padding: 0;
+              li {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width: 150px;
+                height: 25px;
+                a {
+                  font-family: 'roboto', sans-serif;
+                  text-decoration: none;
+                  font-size: 15px;
+                  color: rgba(70, 70, 70, 1);
+                  border-bottom: transparent solid 2px;
+                  :hover {
+                    padding: 10px;
+                    border-bottom: rgba(34, 53, 148, 1) solid 2px;
+                    transition: 0.2s;
+                  }
+                }
+              }
             }
           }
         }
