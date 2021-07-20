8d4e1acbc61175d5bcaeef5809f869918ec385e6
AntoineCau <acauwd@gmail.com>
2021-07-20 15:58:39 +0200

    change style on reco

--- src/components/MarketPlaceReco/styles.jsx
+++ src/components/MarketPlaceReco/styles.jsx
@@ -1,108 +1,60 @@
 import styled from 'styled-components';
 
 const StyledMarketPlaceReco = styled.div`
-  a {
-    text-decoration: none;
-    color: #223594;
-  }
-  .titre {
-    color: #223594;
+  .RecoPage {
+    width: 90vw;
     font-family: 'roboto', sans-serif;
-    font-size: 44px;
-    font-weight: 300;
-    margin-top: 20px;
-    margin-left: 20px;
-  }
-
-  .AppGallery {
-    display: flex;
-    justify-content: space-around;
-    margin-bottom: 50px;
-  }
-
-  .card {
-    margin: 0;
-    cursor: pointer;
-    background-color: white;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    border-radius: 8px;
-    border: solid 1px #c2bcb2;
-    width: 390px;
-    height: 180px;
-    margin-top: 50px;
-    margin-left: 20px;
-    margin-right: 20px;
-    margin-bottom: 20px;
-
-    :hover {
-      transform: scale(1.02);
-      box-shadow: 10px 11px 20px -16px rgba(0, 0, 0, 0.75);
-      -webkit-box-shadow: 10px 11px 20px -16px rgba(0, 0, 0, 0.75);
-      -moz-box-shadow: 10px 11px 20px -16px rgba(0, 0, 0, 0.75);
+    padding-top: 80px;
+    margin-bottom: 30px;
+    margin: auto;
+    color: #223594;
+    .titre {
+      font-size: 44px;
+      font-weight: 300;
+      padding-bottom: 20px;
     }
+    a {
+      text-decoration: none;
+      color: #223594;
+      .AppGallery {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-flow: row wrap;
+        margin-bottom: 30px;
+        margin-top: 10px;
 
-    .image {
-      width: 330px;
-      height: 130px;
-      margin-left: 0;
-      overflow-y: hidden;
-    }
-
-    .imageSize {
-      border-radius: 8px;
-      background-size: cover;
-      max-width: 100%;
-    }
-
-    .infos {
-      display: flex;
-      height: 50px;
-    }
-
-    .societe {
-      border-radius: 8px;
-      display: flex;
-      flex-direction: column;
-      background-color: white;
-      width: 230px;
-
-      p {
-        margin: 0;
-        margin-left: 30px;
-        line-height: 20px;
+        .card {
+          overflow: hidden;
+          cursor: pointer;
+          background-color: white;
+          border-radius: 5px;
+          width: 320px;
+          height: 180px;
+          margin: 20px;
+          position: relative;
+          border: rgba(70, 70, 70, 0.15) solid 1px;
+          box-shadow: -1px -1px 21px 1px rgba(70, 70, 70, 0.15);
+          :hover {
+            border: rgb(247, 143, 33, 0.75) solid 1px;
+          }
+          .imageSize {
+            object-fit: contain;
+            width: 100%;
+            height: 100%;
+          }
+          .name {
+            font-weight: 400;
+            font-size: 18px;
+            padding: 10px;
+            position: absolute;
+            bottom: 0;
+            background-color: #ffff;
+            width: 100%;
+            border-top: rgba(70, 70, 70, 0.15) solid 1px;
+          }
+        }
       }
-
-      .name {
-        font-family: 'roboto', sans-serif;
-        font-weight: 700;
-        font-size: 18px;
-        margin-top: 5px;
-      }
-
-      .activite {
-        font-family: 'roboto', sans-serif;
-        font-weight: 400;
-        font-size: 10px;
-      }
-    }
-
-    .button {
-      background-color: #f78f21;
-      height: 18px;
-      width: 67px;
-      left: 8px;
-      top: 2px;
-      border-radius: 20px;
-      color: #ffffff;
-      font-family: 'roboto', sans-serif;
-      font-weight: 700;
-      font-size: 10px;
-      text-align: center;
-      margin-top: 15px;
-      padding-top: 5px;
-      margin-left: 60px;
     }
   }
 `;
