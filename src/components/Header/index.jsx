549b669d87295328848493ef5a2b28479e06a5cb
gaspard.dechirot <gaspard.balou@gmail.com>
2021-07-26 15:32:01 +0200

    have a button and a new bar to do burger menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import StyledHeader from './styles';
 
 function Header() {
+  const handleClick = true;
+  const burger = true;
   return (
     <StyledHeader>
       <nav className="NavBar">
@@ -34,7 +36,43 @@ function Header() {
             </NavLink>
           </div>
         </div>
+        <button type="button" className="btn-navigation" onClick={handleClick}>
+          <div className="barre" />
+          <div className="barre" />
+          <div className="barre" />
+        </button>
       </nav>
+      <div className={burger ? 'burger' : 'burger1'}>
+        <div className="pages">
+          <ul>
+            <li className="Cockpit">
+              <NavLink to="/cockpit">
+                <p>Cockpit</p>
+              </NavLink>
+            </li>
+            <li className="MyApps">
+              <NavLink to="/myapps">
+                <p>Mes Services</p>
+              </NavLink>
+            </li>
+            <li className="Marketplace">
+              <NavLink to="/apps">
+                <p>Marketplace</p>
+              </NavLink>
+            </li>
+            <li className="Contact">
+              <NavLink to="/contact">
+                <p>Contact</p>
+              </NavLink>
+            </li>
+          </ul>
+          <div className="logoProfile">
+            <NavLink to="/myprofile">
+              <img src="/img/Vector.png" alt="logoProfile" />
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </StyledHeader>
   );
 }
